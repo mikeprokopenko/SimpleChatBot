bc3e2d3e541c6c62d512e84cea7c32e8a032fabe
Mike Prokopenko <mike.prokopenko@ekreative.com>
2020-04-28 14:12:14 +0300

    Completing the first task

--- task/src/bot/SimpleBot.java
+++ task/src/bot/SimpleBot.java
@@ -2,6 +2,16 @@ package bot;
 
 public class SimpleBot {
     public static void main(String[] args) {
-        // write your code here
+        Bot bot = new Bot();
+        bot.printGreetings();
+    }
+}
+
+class Bot {
+    String name = "Aid";
+    int birthYear = 2018;
+
+    public void printGreetings() {
+        System.out.println("Hello! My name is " + name + ".\nI was created in " + birthYear + ".");
     }
 }
